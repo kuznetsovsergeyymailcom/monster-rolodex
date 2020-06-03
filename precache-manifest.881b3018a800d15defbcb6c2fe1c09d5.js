37d6945a6977dff60a5e8deaa63d6f1a35d710e0
sergey <kuznetsovsergey@ymail.com>
2020-06-03 22:08:16 +0300

    Updates

---
+++ precache-manifest.881b3018a800d15defbcb6c2fe1c09d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1e28bb2d646d99f4d6c0ab2b5b43df",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1f2704e5e796ce67ee74",
+    "url": "/monster-rolodex/static/css/main.0638fc02.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2704e5e796ce67ee74",
+    "url": "/monster-rolodex/static/js/main.3a45c134.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
